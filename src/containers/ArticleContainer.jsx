93ff4cfd3ce9bff733bc4fffd54ab0e4b1da2743
Baruch Phillips <baruchphillips@gmail.com>
2020-07-02 16:24:15 -0400

    added mlb tweet timeline

--- src/containers/ArticleContainer.jsx
+++ src/containers/ArticleContainer.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import ListArticles from '../components/ListArticles'
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 class ArticleContainer extends Component {
     render() {
        const listArticles = this.props.articles.map(article => <ListArticles article={article} key={article.id} />)
@@ -9,6 +10,21 @@ class ArticleContainer extends Component {
             <div>
               <strong>Latest Headlines</strong>
                     {listArticles}
+                    <div  style={{
+                position: "absolute",
+                left: '70%',
+                top: '15%'}} className="centerContent" > 
+                      <div className="selfCenter standardWidth">
+                          <TwitterTimelineEmbed
+                              sourceType="profile"
+                              screenName="mlb"
+                              options={{height: 600, width: 400}}
+                              noFooter= 'true'
+                              theme= 'light'
+                            //   onComplete={action}
+                          />
+</div>
+</div>
             </div>
         )
       } else {
