2cc32fc6617f7cb4cb981d645fc847cff65ebdcf
Baruch Phillips <baruchphillips@gmail.com>
2020-10-05 16:06:17 -0400

    added large card must edit css

--- src/containers/ArticleContainer.jsx
+++ src/containers/ArticleContainer.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import ListArticles from '../components/ListArticles'
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
+import SoxPic from '../logos/red_sox.png'
 class ArticleContainer extends Component {
     render() {
        const listArticles = this.props.articles.map(article => <ListArticles article={article} key={article.id} />)
@@ -10,6 +11,14 @@ class ArticleContainer extends Component {
             <div>
               <strong>Headlines may take some time to load</strong>
 
+              {/* <div class="card bg-dark text-white">
+  <img class="card-img" src={SoxPic} alt="Card image" style={{padding-right: '50px'}}></img>
+  <div class="card-img-overlay">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text">Last updated 3 mins ago</p>
+  </div>
+  </div> */}
               {/* Place large article box design here
                */}
                 {listArticles}
